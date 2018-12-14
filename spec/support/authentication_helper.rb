68ec20fe9fb8155304febd08a7403153c071aab6
Abdullah Samman <sammanabdallah@gmail.com>
2018-12-15 00:48:42 +0200

    Revert "Make merged votes voteable"
    This reverts commit 17bdf34667d95268b8558acaa6894ceb36fa103a.

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -1,7 +1,6 @@
 module AuthenticationHelper
   def stub_login_as user
-    random_token = "abcdefg".split('').shuffle.join
-    user.update_column(:session_token, random_token)
-    allow_any_instance_of(ApplicationController).to receive(:session).and_return(u: random_token)
+    user.update_column(:session_token, 'asdf')
+    allow_any_instance_of(ApplicationController).to receive(:session).and_return(u: 'asdf')
   end
 end
